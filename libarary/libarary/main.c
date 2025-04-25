282e452f1414172f2536599709607301d5d55eb2
Madan-2303 <madan.m@anorasolutions.com>
2025-04-25 16:20:46 +0530

    Update main.c with hai printf in remote

--- libarary/libarary/main.c
+++ libarary/libarary/main.c
@@ -1,18 +1,5 @@
-/*
- * libarary.c
- *
- * Created: 25-04-2025 16:06:26
- * Author : Madan M
- */ 
-
-#include <avr/io.h>
-
-
-int main(void)
-{
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
-}
+int main(){
+printf("hai");
+    return 0;
+};
 
